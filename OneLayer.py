4e3f6abeac129a1ca94778d0a6cbbcdfeee9779a
chipgarner <chip@garnertotalenergy.com>
2015-11-06 20:12:46 +0800

    Moved in needed files, OpenCV output, added blur

--- OneLayer.py
+++ OneLayer.py
@@ -2,7 +2,7 @@ import convolver as co
 
 co.use_gpu()
 net = co.load_model()
-img = co.load_image('dianaFits.jpg')
+img = co.load_image('ImagesIn/youandme.jpg')
 
-ending = 'inception_5a/output'
-co.deepdream(net, img, iter_n = 5, end = ending)
+ending = 'inception_5b/output'
+co.deepdream(net, img, iter_n = 100, end = ending)
